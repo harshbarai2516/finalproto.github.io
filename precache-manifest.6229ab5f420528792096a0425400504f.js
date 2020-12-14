9ad62ca21949b77535a8c903b71dfef48db6e70c
Harsh Barai <harshbarai52@gmail.com>
2020-12-14 05:43:00 +0530

    Updates

---
+++ precache-manifest.6229ab5f420528792096a0425400504f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d49a4df28fa84899e09",
+    "url": "/finalproto.github.io/static/css/main.b0ae4ca5.chunk.css"
+  },
+  {
+    "revision": "0d49a4df28fa84899e09",
+    "url": "/finalproto.github.io/static/js/main.ad27ae8c.chunk.js"
+  },
+  {
+    "revision": "5aee055c5164484d3373",
+    "url": "/finalproto.github.io/static/js/runtime~main.0bd632cb.js"
+  },
+  {
+    "revision": "94963522e2726d258138",
+    "url": "/finalproto.github.io/static/js/2.f1714fb5.chunk.js"
+  },
+  {
+    "revision": "461ae141c5088bafdc9e",
+    "url": "/finalproto.github.io/static/js/3.ce65eebf.chunk.js"
+  },
+  {
+    "revision": "eb3c8879c72ba6c99fbc47fecf0fdd99",
+    "url": "/finalproto.github.io/index.html"
+  }
+];
